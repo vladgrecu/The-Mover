82d4137bd09c09b573ece44904cddb46d3c576c0
vladgrecu <51508637+vladgrecu@users.noreply.github.com>
2019-09-16 03:40:06 +0300

    added Timer,StartGame button,finish line, code refactor

--- app.js
+++ app.js
@@ -1,145 +1,133 @@
 class Player{
     constructor(){
         this.domElement = document.getElementById('player');
-        this.obstacles = document.getElementById('obstacles');
+        this.top = 136;
+        this.left = 16;
         this.hearth = document.querySelectorAll('span');
-        console.log(this.hearth);
+        this.life = 3;
+        this.obstacles = document.getElementById('obstacles');
+        this.obstacleLeft = 60;
+        this.arrayOfObstacles = [];
+        for (let i = 0; i < 8; i++){
+            this.arrayOfObstacles.push(this.createObstacles(this.obstacleLeft));
+        }
+        this.listenToDirection();
+        console.log(this.arrayOfObstacles);
+    };
+    tryAgain(){
         this.top = 136;
         this.left = 16;
         this.life = 3;
-        this.obstacleLeft = 40;
-        this.listenToDirection();
-        this.arrayOfObstacles = [];
-        for (let i = 0; i < 8; i++){
-            this.arrayOfObstacles.push(this.createObstacles(this.obstacleLeft,i));
-        }
-        console.log(this.arrayOfObstacles);
-    };
+        this.hearth.forEach(life =>life.style.visibility = 'visible')
+        this.domElement.style.top = this.top;
+        this.domElement.style.left = this.left;
+        alert('You Lose');
+        document.getElementById('timerDisplay').innerText=`Time Left: 30sec`;
+        document.getElementById('startGame').disabled = false;
+    }
+    timer = () => {
+        let sec = 30;
+        let timer = setInterval(()=>{
+            document.getElementById('timerDisplay').innerText=`Time Left: ${sec}sec`;
+            sec--;
+            if (sec!==30){
+                document.getElementById('startGame').disabled = true;
+                document.getElementById('startGame').innerText = 'GO JOHNY GO!!!'
+            }
+            console.log('Timer is:',sec);
+            if (sec < 0) {
+                clearInterval(timer);
+                this.tryAgain();
+                document.getElementById('startGame').innerText = 'Try Again!!!'
+            }
+        }, 1000,this.tryAgain)
+    }
     checkColision(){
         for(let i=0; i<this.arrayOfObstacles.length;i++){
             let obstacle = this.arrayOfObstacles[i];
-            if(obstacle.top === 0){
-                //VERIFICA OBSTACOLELE CARE PORNESC DE SUS
-                if((this.left >= obstacle.left - 15)&&(this.left <= obstacle.left+15)&&(this.top <= obstacle.height)){
-                    console.log(`Colission with obstacle #${i+1}`);
-                    console.log(obstacle);
-                    if (this.life===0){
-                        alert('You Lose');
-                        this.top = 136;
-                        this.left = 16;
-                        this.domElement.style.top = this.top;
-                        this.domElement.style.left = this.left;
-                        this.life = 3;
-                        for(let i = 0; i<=3; i++){
-                            this.hearth[i].style.visibility = 'visible';
-                        }
-                    }
-                    console.log('Vieti: ',this.life-1);
-                    return true
-                } 
-            } else {
-                //VERIFICA OBSTACOLELE CARE PORNESC DE JOS
-                if((this.left >= obstacle.left - 15)&&(this.left <= obstacle.left+15)&&(this.top+15 >= obstacle.top)){
-                    console.log(`Colission with obstacle #${i+1}`);
-                    console.log(obstacle);
-                    if (this.life===0){
-                        alert('You Lose');
-                        this.top = 136;
-                        this.left = 16;
-                        this.domElement.style.top = this.top;
-                        this.domElement.style.left = this.left;
-                        this.life = 3;
-                        for(let i = 0; i<=3; i++){
-                            this.hearth[i].style.visibility = 'visible';
-                        }
-                    }
-                    console.log('Vieti: ',this.life-1);
-                    return true
-            }}
+            if((this.left >= obstacle.left - 15)                          //Verifica Coliziune Stanga
+            &&(this.left <= obstacle.left+15)                            //Verifica Coliziune Dreapta
+            &&((obstacle.top === 0 && this.top <= obstacle.height)      //Verifica daca este coliziune cu obstacol care porneste de sus SAU
+            ||(obstacle.top !== 0 && this.top+15 >= obstacle.top))) {   //cu obstacol care porneste de jos
+                this.life--;
+                console.log(`Colission with obstacle #${i+1}`);
+                this.hearth[this.life].style.visibility = 'hidden';
+                if (this.life === 0){
+                    setTimeout(() => {
+                        this.tryAgain();
+                    }, 0);       
+                }
+                return true
+            }     
         }
         return false
     }
-    createObstacles(left,i){
+    createObstacles(left){
         let obstacle = document.createElement('div');
-        let obstacleObject = {};
         obstacle.classList.add('obstacle');
-        obstacle.id = 'o' + i;
-        obstacle.style.height = Math.floor(Math.random()*235)+50;
+        obstacle.style.left=left;
+        obstacle.style.height = Math.floor(Math.random()*230)+50;
         let topOrBottom=[0, 300 - parseInt(obstacle.style.height)];
         obstacle.style.top = topOrBottom[Math.floor(Math.random()*2)];
-        obstacle.style.left=left;
-        this.obstacleLeft = left+33+Math.floor(Math.random()*50);
         this.obstacles.appendChild(obstacle);
+        
+        let obstacleObject = {};
         obstacleObject.height = parseInt(obstacle.style.height);
         obstacleObject.top = parseInt(obstacle.style.top);
         obstacleObject.left = parseInt(obstacle.style.left);
-        return obstacleObject
-        }
         
+        this.obstacleLeft = left+33+Math.floor(Math.random()*50);
+        return obstacleObject
+    }
     listenToDirection(){
         document.addEventListener('keydown', this.movePlayer);
+        document.getElementById('startGame').addEventListener('click', this.timer);
     }
     movePlayer = (event) =>{
         if (event.key === "ArrowUp"){
             this.top -=2;
+            this.domElement.style.top =this.top;
             if(this.checkColision()){
-                this.life--;
                 this.top+=2;
                 this.domElement.style.top=this.top;
-                this.hearth[this.life].style.visibility = 'hidden';
             };
-            console.log('PlayerTop = ', this.top);
-            console.log('PlayerLeft =', this.left);
             if(this.top <= 2){
-                this.top = 0;
-            }
-            this.domElement.style.top =this.top;
+                this.top = 2;
+            };
         }
         if (event.key === "ArrowDown"){
             this.top +=2;
+            this.domElement.style.top =this.top;
             if(this.checkColision()){
-                this.life--;
                 this.top-=2;
                 this.domElement.style.top=this.top;
-                this.hearth[this.life].style.visibility = 'hidden';
             };
-            console.log('PlayerTop = ', this.top);
-            console.log('PlayerLeft =', this.left);
-            if(this.top >= 284){
-                this.top = 286;
-            }
-            this.domElement.style.top =this.top;
+            if(this.top >= 282){
+                this.top = 282;
+            };
         }
         if (event.key === "ArrowLeft"){
             this.left -=2;
+            this.domElement.style.left =this.left;
             if(this.checkColision()){
-                this.life--;
                 this.left+=2;
                 this.domElement.style.left=this.left;
-                this.hearth[this.life].style.visibility = 'hidden';
             };
-            console.log('PlayerTop = ', this.top);
-            console.log('PlayerLeft =', this.left);
             if(this.left <= 2){
-                this.left = 0;
+                this.left = 2;
             }
-            this.domElement.style.left =this.left;
         }
         if (event.key === "ArrowRight"){
             this.left +=2;
+            this.domElement.style.left =this.left;
             if(this.checkColision()){
-                this.life--;
                 this.left-=2;
                 this.domElement.style.left=this.left;
-                this.hearth[this.life].style.visibility = 'hidden';
             };
-            console.log('PlayerTop = ', this.top);
-            console.log('PlayerLeft =', this.left);
             if(this.left >= 586){
                 alert('You Win!!!');
                 location.reload();
             }
-            this.domElement.style.left =this.left;
         }
     }
 }
